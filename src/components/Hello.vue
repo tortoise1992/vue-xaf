1189da4ecc1af85935c880114f1223708e1ee902
ahui666 <772084015@qq.com>
2017-09-16 17:23:47 +0800

    初始化

---
+++ src/components/Hello.vue
@@ -0,0 +1,53 @@
+<template>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      
+    </el-table>
+  </template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      tableData:[]
+    }
+  },
+  created () {
+    this.$http.get(this.$mock+'/test').then((data)=>{
+      if(data){
+        this.tableData=data.data.test
+      }
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import router from './router'
+import axios from 'axios'
+import mock from '../config/mock'
+
+Vue.prototype.$http = axios
+Vue.prototype.$mock=mock
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
